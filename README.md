42974b09c7ff82c4d68395043cc24ffa53d694f0
duajaradat <duajaradat164@gmail.com>
2021-08-26 01:36:42 +0300

    post method lab13

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Project Name
 
-**Author**: Abdullah ,saddam ,mansour
+**Author**: Abdullah ,saddam 
 **Version**: 2.0.0 (increment the patch/fix version number if you make more commits past your first submission)
 
 ## Overview
--- server.js
+++ server.js
@@ -3,12 +3,14 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const { default: axios } = require('axios');
 require('dotenv').config();
 
 const PORT = process.env.PORT;
 
 const server = express();
 server.use(cors());
+server.use(express.json());
 
 
 //books database
@@ -16,11 +18,10 @@ mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useU
 
 //Schema 
 const bookSchema = new mongoose.Schema({
-    books: [{
-        title: String,
-        description: String,
-    }],
     email: String,
+    title: String,
+    description: String,
+
 });
 
 //Model 
@@ -29,36 +30,33 @@ const BooksModel = mongoose.model('Books', bookSchema);
 function seedDataCollection() {
     const emailYahoo = new BooksModel({
         email: "duajaradat164@yahoo.com",
-        books: [
-            {
-                title: "Idiot Brain",
-                description: "Explains What Your Head is Really Up To",
-                status: "success",
-            },
-            {
-                title: "Happy Brain",
-                description: "investigate what causes happiness, where it comes from, and why we are so desperate to hang onto it.",
-                status: "success",
-            }
-
-        ]
-
+        title: "Idiot Brain",
+        description: "Explains What Your Head is Really Up To",
+        status: "success",
     })
+    const emailYahoo2 = new BooksModel({
+        email: "duajaradat164@yahoo.com",
+        title: "Happy Brain",
+        description: "investigate what causes happiness, where it comes from, and why we are so desperate to hang onto it.",
+        status: "success",
+    })
+
 
     const emailGmail = new BooksModel({
         email: "duajaradat164@gmail.com",
-        books: [{
-            title: "The Da Vinci Code",
-            description: "mystery thriller novel.",
-            status: "complete"
-        }]
+        title: "The Da Vinci Code",
+        description: "mystery thriller novel.",
+        status: "complete"
+
+
 
     })
     emailYahoo.save();
+    emailYahoo2.save();
     emailGmail.save();
-
 }
-// seedDataCollection();
+
+seedDataCollection();
 
 //localhost:3001/books?email=duajaradat164@gmail.com
 server.get('/books', getBooksHandler);
@@ -69,8 +67,29 @@ function getBooksHandler(req, res) {
         if (err) {
             console.log('There is no Data for the email address: ' + emailAdress);
         } else {
-            console.log("yyyyyyyyyyyyyyyy", resultData[0].books);
-            res.send(resultData[0].books);
+            console.log("yyyyyyyyyyyyyyyy", resultData);
+            res.send(resultData);
+        }
+    })
+}
+
+
+//localhost:3001/addbook?email={user.email}
+server.post('/addbook', addBookHandler);
+async function addBookHandler(req, res) {
+    console.log('addBook');
+    console.log(req.body);
+    // let { email, title, description, status } = req.body;
+    const newBook = new BooksModel(
+        req.body
+    )
+    await newBook.save();
+    BooksModel.find({ email: req.body.email }, function (err, resultData) {
+        if (err) {
+            console.log('There is no Data for the email address: ' + req.body.email);
+        } else {
+            console.log("yyyyyyyyyyyyyyyy", resultData);
+            res.send(resultData);
         }
     })
 }
@@ -78,6 +97,7 @@ function getBooksHandler(req, res) {
 //listening on
 server.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
+
 
 })
 
